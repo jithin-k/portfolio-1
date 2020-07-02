cf543442c97525cb815c1bf9d3b06521f147c8ff
Catherine George <catherine@qburst.com>
2020-07-02 17:11:51 +0530

    Porfolio initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InfoBarComponent } from './info-bar/info-bar.component';
+import { ContentComponent } from './content/content.component';
+import { MenuBarComponent } from './menu-bar/menu-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InfoBarComponent,
+    ContentComponent,
+    MenuBarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
