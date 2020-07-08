32d6eb28c718d91cbfbf97ae49658dac4e4746dc
Catherine George <catherine@qburst.com>
2020-07-08 12:34:24 +0530

    Projects masonary layout added

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -10,6 +10,20 @@ export class ProjectComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.masonryLayoutHeightSetUp();
+  }
+
+  masonryLayoutHeightSetUp() {
+    const numCols = 3;
+    const colHeights = Array(numCols).fill(0);
+    const container = document.getElementById('masonry-with-columns');
+
+    Array.from(container.children as HTMLCollectionOf<HTMLElement>).forEach((child, i) => {
+      const order = i % numCols;
+      child.style.order = order as any;
+      colHeights[order] += parseFloat(child.clientHeight as any);
+    })
+    container.style.height = Math.max(...colHeights) + 'px';
   }
 
 }
