12d580bbbc156b93b0e928ad1cc89c6ed7dcdbe6
CatherinKG <catherinekg38@gmail.com>
2020-08-12 13:21:08 +0530

    Expertise added

--- src/app/my-services/my-services.component.ts
+++ src/app/my-services/my-services.component.ts
@@ -1,6 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { MyServices } from 'src/app/models/my-services.model'
-
+import { faHeartbeat } from '@fortawesome/free-solid-svg-icons';
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { faAd } from '@fortawesome/free-solid-svg-icons';
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faBluetooth } from "@fortawesome/free-brands-svg-icons";
+import { faAws } from "@fortawesome/free-brands-svg-icons";
+import { faTwitter } from "@fortawesome/free-brands-svg-icons";
 @Component({
   selector: 'app-my-services',
   templateUrl: './my-services.component.html',
@@ -9,6 +15,15 @@ import { MyServices } from 'src/app/models/my-services.model'
 export class MyServicesComponent implements OnInit {
 
   @Input() public myServices: MyServices;
+
+  cart = faShoppingCart;
+  health = faHeartbeat;
+  ads = faAd;
+  google = faGoogle;
+  bluetooth = faBluetooth;
+  aws = faAws;
+  twitter = faTwitter;
+
 
   constructor() { }
 
